7d0f33838c899b05b09a491f32adba894888e5aa
young92117 <young92117@gmail.com>
2019-02-04 12:56:10 -0800

    refactored  router structure

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ import cookieParser from 'cookie-parser'
 import express from 'express'
 import helmet from 'helmet'
 import morgan from 'morgan'
-import { userRouter } from './router'
+import userRouter from './routers/userRouter'
+import videoRouter from './routers/videoRouter'
+import globalRouter from './routers/globalRouter'
+import routes from './routes'
 
 const app = express()
-
-const handleHome = (req, res) => res.send('Hello from Young')
-
-const handleProfile = (req, res) => res.send('You are on my profile')
 
 app.use(cookieParser())
 app.use(bodyParser.json())
@@ -18,9 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(helmet())
 app.use(morgan('dev'))
 
-app.get('/', handleHome)
-
-app.get('/profile', handleProfile)
-app.use('/user', userRouter)
+app.use(routes.home, globalRouter)
+app.use(routes.users, userRouter)
+app.use(routes.videos, videoRouter)
 
 export default app;
---
+++ routers/userRouter.js
@@ -0,0 +1,12 @@
+import express from  'express'
+import routes from '../routes'
+
+const userRouter = express.Router()
+userRouter.get(routes.home, (req,res) => res.send('Users'))
+userRouter.get(routes.userDetail, (req,res) => res.send('Users Detail'))
+userRouter.get(routes.editProfile, (req,res) => res.send('Edit profile'))
+userRouter.get(routes.changePassword, (req,res) => res.send('Change Password'))
+
+
+export default userRouter
+
