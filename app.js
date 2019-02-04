be134c5eb1b21879f9808160b613823e9611c19f
young92117 <young92117@gmail.com>
2019-02-03 20:37:05 -0800

    adding the router

---
+++ app.js
@@ -0,0 +1,26 @@
+// const express = require('express')
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { userRouter } from './router'
+
+const app = express()
+
+const handleHome = (req, res) => res.send('Hello from Young')
+
+const handleProfile = (req, res) => res.send('You are on my profile')
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(helmet())
+app.use(morgan('dev'))
+
+app.get('/', handleHome)
+
+app.get('/profile', handleProfile)
+app.use('/user', userRouter)
+
+export default app;
