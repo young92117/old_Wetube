b8a576fd052684c8fcd142c2bc5598fbdaa7f224
young92117 <young92117@gmail.com>
2019-02-05 21:59:27 -0800

    change the order of router so that id does not match everything

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,9 +3,9 @@ import routes from '../routes'
 
 const userRouter = express.Router()
 userRouter.get(routes.home, (req,res) => res.send('Users'))
-userRouter.get(routes.userDetail, (req,res) => res.send('Users Detail'))
 userRouter.get(routes.editProfile, (req,res) => res.send('Edit profile'))
 userRouter.get(routes.changePassword, (req,res) => res.send('Change Password'))
+userRouter.get(routes.userDetail, (req,res) => res.send('Users Detail'))
 
 
 export default userRouter
